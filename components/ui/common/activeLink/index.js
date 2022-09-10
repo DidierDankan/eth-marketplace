97eb38ab41e642b057843ccc3e297aec5aa692d3
DidierDankan <didi_archer@hotmail.com>
2022-09-10 16:44:41 +0200

    cleanup

--- components/ui/common/activeLink/index.js
+++ components/ui/common/activeLink/index.js
@@ -6,13 +6,8 @@ export default function ActiveLink({ children, ...props }) {
 	const { pathname } = useRouter();
 	let className = children.props.className || '';
 
-	console.log('pathname', pathname);
-	console.log('PROPS.HREF', props.href);
-
 	if (pathname === props.href) {
-		console.log('inside');
 		className = `${className} text-yellow-500`;
-		console.log('className', className);
 	}
 
 	return <Link {...props}>{React.cloneElement(children, { className })}</Link>;
