505cb177c86990b6d6f754f9877ae34a3d96822e
DidierDankan <didi_archer@hotmail.com>
2022-09-22 17:45:12 +0200

    eth mainet prepared

--- truffle-config.js
+++ truffle-config.js
@@ -29,6 +29,27 @@ module.exports = {
 			networkCheckTimeout: 1000000,
 			timeoutBlocks: 200, // number of blockas before deployment times out
 		},
+		//ethereum mainet
+		live: {
+			// must be a thunk, otherwise truffle commands may hang in CI
+			provider: () =>
+				new HDWalletProvider({
+					mnemonic: {
+						//cant share this with no one
+						phrase: keys.MNEMONIC,
+					},
+					providerOrUrl: `wss://mainet.infura.io/ws/v3/${keys.INFURA_PROJECT_ID}`,
+					//choosing the first account in our wallet
+					addressIndex: 0,
+				}),
+			network_id: 1,
+			gas: 2500000, // Gas limit, how much gas wer willing to spent. if gas used is less, the rest will return to address
+			gasPrice: 710000000000, // How much we are willing to spent for unit of gas (20 gwei)
+			confirmations: 2, // number of blocks to wait between deployement
+			networkCheckTimeout: 1000000,
+			timeoutBlocks: 200, // number of blockas before deployment times out
+			skipDryRun: true,
+		},
 	},
 	compilers: {
 		solc: {
